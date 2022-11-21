bb0b76f2649c221efb83b0dfc290fc49637f2b17
Tom <tommy.seng.heng@gmail.com>
2022-11-21 01:52:20 -0500

    Started adding typescript

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,38 @@
-import React from "react";
-import "./Navbar.css";
+import React from 'react';
+import './Navbar.css';
 import Pdf from '../../assets/tommy-heng-resume.pdf';
 
 /**
  * Creates a navbar
  */
-const Navbar = props => {
+const Navbar = (props) => {
   return (
-    <nav className='navbar'>
-      <div className='navbar-content'>
+    <nav className="navbar">
+      <div className="navbar-content">
         {/* The Navbar Links */}
-        <div className='navbar-links'>
-          <a href='#banner'><div className='navbar-link'>HOME</div></a>
-          <a href='#about'><div className='navbar-link'>ABOUT</div></a>
-          <a href='#experience'><div className='navbar-link'>EXPERIENCE</div></a> 
-          <a href='#projects'><div className='navbar-link'>PORTFOLIO</div></a> 
+        <div className="navbar-links">
+          <a href="#banner">
+            <div className="navbar-link">HOME</div>
+          </a>
+          <a href="#about">
+            <div className="navbar-link">ABOUT</div>
+          </a>
+          <a href="#experience">
+            <div className="navbar-link">EXPERIENCE</div>
+          </a>
+          <a href="#projects">
+            <div className="navbar-link">PORTFOLIO</div>
+          </a>
         </div>
         {/* Resume Button in the top right */}
-        <a 
-          href={Pdf} 
-          target='_blank' 
-          rel='noreferrer' 
-          className='outline-button'
-        >RESUME</a>
+        <a
+          href={Pdf}
+          target="_blank"
+          rel="noreferrer"
+          className="outline-button"
+        >
+          RESUME
+        </a>
       </div>
     </nav>
   );
